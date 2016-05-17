c8e89467b781af1b6641619569a7656e42a6e657
Hoye <hoyelam96@gmail.com>
2016-05-17 13:15:50 +0200

    start authentication unit tests

---
+++ Smart Mirror App/Smart Mirror App Unit Tests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using Smart_Mirror_App.authentication;
+namespace Smart_Mirror_App_Unit_Tests
+{
+    [TestClass]
+    public class UnitTestsAuthentication
+    {
+        [TestMethod]
+        public void GetToken_Test()
+        {
+            var googleAuthentication = new GoogleAuthentication();
+            var token = googleAuthentication.GetToken();
+            Assert.AreEqual("", "");
+        }
+    }
+}
