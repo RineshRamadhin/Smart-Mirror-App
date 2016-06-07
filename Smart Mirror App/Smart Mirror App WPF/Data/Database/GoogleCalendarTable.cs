b7f8187ce88f5f22c31fee3a1118e928321beac6
Hoye <hoyelam96@gmail.com>
2016-06-07 16:21:17 +0200

    start putting calendar data in DB

---
+++ Smart Mirror App/Smart Mirror App WPF/Data/Database/GoogleCalendarTable.cs
@@ -0,0 +1,32 @@
+﻿using Smart_Mirror_App_WPF.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Mirror_App_WPF.Data.Database
+{
+    public class GoogleCalendarTable : DefaultDatabaseTable<GoogleCalendarModel>
+    {
+        public GoogleCalendarTable()
+        {
+            this.CreateTable();
+        }
+
+        public override GoogleCalendarModel GetRow(string primaryKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertRow(GoogleCalendarModel model)
+        {
+            database.Insert(model);
+        }
+
+        protected override void UpdateRow(GoogleCalendarModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
