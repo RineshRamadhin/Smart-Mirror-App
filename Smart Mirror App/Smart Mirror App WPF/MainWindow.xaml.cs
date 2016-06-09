8c24f60d09f1233f3b23a32f8a7dde3774332efe
Tjarda112 <tjardavoorn@hotmail.com>
2016-06-09 17:07:21 +0200

    Changed layout in mainwindow

--- Smart Mirror App/Smart Mirror App WPF/MainWindow.xaml.cs
+++ Smart Mirror App/Smart Mirror App WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Smart_Mirror_App_WPF.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Smart_Mirror_App_WPF
 {
@@ -20,9 +23,29 @@ namespace Smart_Mirror_App_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            startclock();
+           
         }
+
+        private void startclock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start(); 
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            //  throw new NotImplementedException();
+            time.Text = DateTime.Now.ToString("H:mm");
+            date.Text = DateTime.Today.ToString("M", CultureInfo.CreateSpecificCulture("nl-BE"));
+        }
+        
+       
     }
 }
--- Smart Mirror App/Smart Mirror App WPF/View/ClockView.xaml.cs
+++ Smart Mirror App/Smart Mirror App WPF/View/ClockView.xaml.cs
@@ -26,7 +26,7 @@ namespace Smart_Mirror_App_WPF.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ClockView : Page
+    public sealed partial class ClockView : Grid
     {
         private string city;
 
