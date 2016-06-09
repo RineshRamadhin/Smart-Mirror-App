350a78716077d00ba7950f42728247dca3db585d
Hoye <hoyelam96@gmail.com>
2016-06-09 17:24:10 +0200

    start with google data correlator

---
+++ Smart Mirror App/Smart Mirror App WPF/Data/Bot/GoogleDataCorrelator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Mirror_App_WPF.Data.Bot
+{
+    public class GoogleDataCorrelator
+    {
+        public GoogleDataCorrelator()
+        {
+
+        }
+
+        public string CorrelateCalendarWithGmail()
+        {
+
+            return "";
+        }
+    }
+}
