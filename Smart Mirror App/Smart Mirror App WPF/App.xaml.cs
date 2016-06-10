7f199f53fa3e1045684933c060a2661ad1120376
Hoye <hoyelam96@gmail.com>
2016-06-10 14:09:31 +0200

    Revert "Merge branch 'master' of https://github.com/RineshRamadhin/Smart-Mirror-App"
    This reverts commit 519be73ec3362bf7142f4da61be8d59ea2486442, reversing
    changes made to 4de261a8269cce75c8b8b1b546e90a5ced6fe42c.

--- Smart Mirror App/Smart Mirror App WPF/App.xaml.cs
+++ Smart Mirror App/Smart Mirror App WPF/App.xaml.cs
@@ -1,42 +1,20 @@
-﻿using Smart_Mirror_App_WPF.View;
-using Smart_Mirror_App_WPF.Controller;
-using Smart_Mirror_App_WPF.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Configuration;
+using System.Data;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Smart_Mirror_App_WPF.ViewModel;
+using Owin;
+using Microsoft.Owin.Security.Google;
 
 namespace Smart_Mirror_App_WPF
 {
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    /// Interaction logic for App.xaml
     /// </summary>
-    sealed partial class App : Application
+    public partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-            var clockModel = new ClockModel();
-            clockModel.Update();
-            TimerController.RegisterModel(clockModel);
-            (Resources["clock"] as ClockView).InitializeComponent();
 
-        }
-
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        /// 
     }
 }
--- Smart Mirror App/Smart Mirror App WPF/MainWindow.xaml.cs
+++ Smart Mirror App/Smart Mirror App WPF/MainWindow.xaml.cs
@@ -1,26 +1,9 @@
-﻿using Smart_Mirror_App_WPF.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-<<<<<<< HEAD
 using Smart_Mirror_App_WPF.Util;
 using System;
 using Smart_Mirror_App_WPF.Input.Motion.LeapMotion;
-=======
-using System.Windows.Shapes;
-using System.Windows.Threading;
->>>>>>> 9d93b36e497c1aee576ff231c191d59bfe02f0fc
 
 namespace Smart_Mirror_App_WPF
 {
@@ -29,33 +12,38 @@ namespace Smart_Mirror_App_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ICommand _navigateToAuthenticationPage;
 
         public MainWindow()
         {
             InitializeComponent();
-            startclock();
-           
+            NavigateToPage();
         }
 
-        private void startclock()
+        public ICommand NavigateToAuthenticationPage
         {
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += tickevent;
-            timer.Start(); 
+            get
+            {
+                if (_navigateToAuthenticationPage == null)
+                {
+                    _navigateToAuthenticationPage = new RelayCommand(
+                        param => this.NavigateToPage(),
+                        param => this.CanNavigate()
+                    );
+                }
+                return _navigateToAuthenticationPage;
+            }
         }
 
-        private void tickevent(object sender, EventArgs e)
+        private void NavigateToPage()
         {
-            //  throw new NotImplementedException();
-            time.Text = DateTime.Now.ToString("H:mm");
-            date.Text = DateTime.Today.ToString("M", CultureInfo.CreateSpecificCulture("nl-BE"));
+            Authentication.Google.AuthenticationPage nextPage = new Authentication.Google.AuthenticationPage();
+            MainFrame.Navigate(nextPage);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private bool CanNavigate()
         {
-            new SettingsWindow().Show();
-
+            return true;
         }
     }
 }
